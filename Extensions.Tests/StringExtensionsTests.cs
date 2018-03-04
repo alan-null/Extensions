31c3453b3595490032e46b6cc6a68238969147e2
Alan Płócieniak <alan-null@outlook.com>
2018-03-04 13:08:50 +0100

    Added StringExtensions

--- Extensions.Tests/StringExtensionsTests.cs
+++ Extensions.Tests/StringExtensionsTests.cs
@@ -1,4 +1,6 @@
-using System;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
 using Xunit;
 
 namespace Extensions.Tests
@@ -6,14 +8,81 @@ namespace Extensions.Tests
     public class StringExtensionsTests
     {
         [Theory]
+        [InlineData(null, true)]
+        [InlineData("", true)]
+        [InlineData(" ", false)]
+        [InlineData(" s ", false)]
+        [InlineData("s", false)]
+        public void IsNullOrEmpty_Test(string input, bool expected)
+        {
+            Assert.Equal(expected, input.IsNullOrEmpty());
+        }
+
+        [Theory]
         [InlineData(null, false)]
         [InlineData("", false)]
         [InlineData(" ", false)]
         [InlineData(" s ", true)]
         [InlineData("s", true)]
-        public void NotNullOrWhiteSpace(string input, bool expected)
+        public void NotNullOrWhiteSpace_Test(string input, bool expected)
         {
             Assert.Equal(expected, input.NotNullOrWhiteSpace());
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(" ", true)]
+        [InlineData(" s ", true)]
+        [InlineData("s", true)]
+        public void NotNullOrEmpty_Test(string input, bool expected)
+        {
+            Assert.Equal(expected, input.NotNullOrEmpty());
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(" ", false)]
+        [InlineData(" s ", true)]
+        [InlineData("s", true)]
+        public void HasValue_Test(string input, bool expected)
+        {
+            Assert.Equal(expected, input.HasValue());
+        }
+
+        [Fact]
+        public void RemoveDiacritics_Test()
+        {
+            // 'Cyrillic' is not a supported encoding name. 
+            // https://github.com/dotnet/corefx/issues/9158
+            // https://msdn.microsoft.com/en-us/library/mt643901(v=vs.110).aspx
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            string input = "ą,Ą,ć,Ć,ę,Ę,ł,Ł,ń,Ń,ó,Ó,ś,Ś,ż,Ż,ź,Ź";
+            string expected = "a,A,c,C,e,E,l,L,n,N,o,O,s,S,z,Z,z,Z";
+            Assert.Equal(expected, input.RemoveDiacritics());
+        }
+
+        [Theory]
+        [InlineData("a=1&b=2")]
+        [InlineData("?a=1&b=2")]
+        public void ParseQueryString_Test(string input)
+        {
+            var collection = input.ParseQueryString();
+            Assert.Equal(2, collection.Count);
+            Assert.Equal(new NameValueCollection { { "a", "1" }, { "b", "2" } }, collection);
+            Assert.Equal("a=1&b=2", collection.ToString());
+        }
+
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("", "")]
+        [InlineData("test", "Test")]
+        [InlineData("Test", "Test")]
+        public void ToUpperFirst_Test(string input, string expected)
+        {
+            Assert.Equal(expected, input.ToUpperFirst());
         }
     }
 }
--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,12 +1,64 @@
-﻿using static System.String;
+﻿using System;
+using System.Collections.Specialized;
+using System.Text;
+using System.Web;
+using static System.String;
 
 namespace Extensions
 {
     public static class StringExtensions
     {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
         public static bool NotNullOrWhiteSpace(this string str)
         {
             return !IsNullOrWhiteSpace(str);
+        }
+
+        public static bool NotNullOrEmpty(this string str)
+        {
+            return !IsNullOrEmpty(str);
+        }
+
+        public static bool HasValue(this string input)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+            if (input.Trim() == Empty)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static string RemoveDiacritics(this string s)
+        {
+            string asciiEquivalents = Encoding.ASCII.GetString(
+                Encoding.GetEncoding("Cyrillic").GetBytes(s)
+            );
+
+            return asciiEquivalents;
+        }
+
+        public static NameValueCollection ParseQueryString(this string str)
+        {
+            return HttpUtility.ParseQueryString(str);
+        }
+
+        public static string ToUpperFirst(this string str)
+        {
+            if (IsNullOrEmpty(str))
+            {
+                return Empty;
+            }
+            char[] array = str.ToCharArray();
+            array[0] = Char.ToUpper(array[0]);
+            return new string(array);
         }
     }
 }
