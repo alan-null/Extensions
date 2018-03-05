d7568d29fbf628a42639b56d74fc8ba8b8336a56
Alan Płócieniak <alan-null@outlook.com>
2018-03-05 13:12:08 +0100

    Added GenericExtensions

---
+++ Extensions.Tests/GenericExtensionsTests.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace Extensions.Tests
+{
+    public class GenericExtensionsTests
+    {
+        [Fact]
+        public void SafeGet_From_Dictionary_Test()
+        {
+            var obj = new Dictionary<string, int> { { "a", 1 } };
+            Assert.Equal(1, obj.SafeGet(x => x["a"]));
+            Assert.Equal(0, obj.SafeGet(x => x["b"]));
+        }
+
+        [Fact]
+        public void SafeGet_From_Object_Test()
+        {
+            var obj = new Model { Number = 1 };
+            Assert.Null(obj.SafeGet(x => x.Nested.Number));
+            Assert.Equal(1, obj.SafeGet(x => x.Number));
+        }
+
+        [Fact]
+        public void SafeGet_From_Collection_Test()
+        {
+            var obj = new List<int> { 1, 2, 3 };
+            Assert.Equal(1, obj.SafeGet(x => x[0]));
+            Assert.Equal(0, obj.SafeGet(x => x[10]));
+        }
+    }
+
+    internal class Model
+    {
+        public Nested Nested { get; set; }
+        public int Number { get; set; }
+    }
+
+    internal class Nested
+    {
+        public string Number { get; set; }
+    }
+}
---
+++ Extensions/GenericExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class GenericExtensions
+    {
+        public static TOut SafeGet<TOut, TIn>(this TIn obj, Func<TIn, TOut> func)
+        {
+            if (obj != null)
+            {
+                try
+                {
+                    return func(obj);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            return default(TOut);
+        }
+    }
+}
